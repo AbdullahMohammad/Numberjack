392f2f85f9e585dea0f2dc12abe2a9864960c99a
Abdullah Mohammad <AbdullahMohammad00@gmail.com>
2014-07-21 16:59:41 +0100

    New problems and fixes

--- examples/CSPLibNJSolutions/044-SteinerTripleSystems.py
+++ examples/CSPLibNJSolutions/044-SteinerTripleSystems.py
@@ -1,35 +1,28 @@
 # Steiner Triple Systems
 
-# Find a set of n(n−1)/6 triples of distinct integer elements in {1,...,n} such that any two triples have at most one common element.
+# Find a set of n(n-1)/6 triples of distinct integer elements in {1,...,n} such that any two triples have at most one common element.
 
-# CSPLib Problem 023 - http://www.csplib.org/Problems/prob044/
+# CSPLib Problem 044 - http://www.csplib.org/Problems/prob044/
 
 from Numberjack import *
 
 def get_model(N):
     N_ROWS = N * (N - 1) / 6
     
-    sol = Matrix[N_ROWS, 3, 1, N]
-    
-    cards = {}
+    sol = Matrix(N_ROWS, N, 0, 1)
 
-    for i in range(1, N + 1):
-        cards[i] = 2
-
-    model = Model()
-
-    # This model will not work for the problem.
-            
+    model = Model([Sum(row) == 3 for row in sol.row])
+        
     for i in range(N_ROWS):
-        for j in range(i + 1, N_ROWS): 
-            model += Gcc(sol.row[i] + sol.row[j], cards)
-
+        for j in range(i, N_ROWS):
+                model += Sum([sol[i][k] == 1 and sol[j][k] == 1 for k in range(N)]) <= 1
+    
     return sol, model
 
 def solve(param):
     N = param['N']
 
-    sol, model = get_model()
+    sol, model = get_model(N)
 
     solver = model.load(param['solver'])
     solver.setVerbosity(param['verbose'])
